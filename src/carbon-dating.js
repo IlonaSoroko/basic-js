d1e7166cd16f34d25a7533cbbdc7713ffa5b131f
Ilona Soroko <ilona.rumak7@gmail.com>
2020-03-03 00:14:16 +0300

    feat: solve Count Cats and Carbon Dating tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,20 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const RATE_CONSTANT = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) === 'string') {
+    sampleActivity = parseFloat(sampleActivity);
+    if (!Number.isNaN(sampleActivity) && sampleActivity > 0 && sampleActivity <=15) {
+    let number = (Math.log(MODERN_ACTIVITY/sampleActivity))/(RATE_CONSTANT/HALF_LIFE_PERIOD);
+    return(Math.ceil(number)); 
+    }
+    else {
+      return false;
+    }
+  }
+  else {
+    return false;
+  }
+}
