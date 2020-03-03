9419a2f5bb059f88ef309dca655f4d417c13ae75
Ilona Soroko <ilona.rumak7@gmail.com>
2020-03-03 21:54:22 +0300

    feat: solve Dream Team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let name = [];
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof(members[i]) == 'string') {
+        members[i] = members[i].trim().toUpperCase();
+        name.push(members[i][0]);
+      }
+      else {
+        continue;
+      }
+    }
+    return name.sort().join('');
+  }
+  else {
+    return false;
+  }
 };
